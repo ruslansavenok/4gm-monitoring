c3fc17f1a036629f19f8ef0df02ecfdaecca60b7
Ruslan Savenok <ruslan.savenok@gmail.com>
2026-01-03 17:17:25 +0100

    Cleaner code

--- data_processor/index.ts
+++ data_processor/index.ts
@@ -2,30 +2,33 @@ import * as api from "./rpc_client";
 import { setupMongoConnection } from "../db/connection";
 import { PrivateListing, generateListingId } from "../db/models/PrivateListing";
 
+async function fetchAndProcessListings(serverId: number, itemId: number) {
+  const rawListings = await api.gameSignalPredicate({ serverId, itemId });
+  await PrivateListing.bulkWrite(
+    rawListings.map((item) => {
+      const doc = { serverId, itemId, ...item };
+      const _id = generateListingId(doc);
+      return {
+        updateOne: {
+          filter: { _id },
+          update: { $setOnInsert: { _id, ...doc } },
+          upsert: true,
+        },
+      };
+    }),
+    { ordered: false },
+  );
+  return rawListings;
+}
+
 (async () => {
   await setupMongoConnection();
   console.log("Mongo connected!");
 
   api.client.on("open", async () => {
-    const serverId = 45;
-    const itemId = 48493;
-    const rawListings = await api.gameSignalPredicate({ serverId, itemId });
-    await PrivateListing.bulkWrite(
-      rawListings.map((item) => {
-        const doc = { serverId, itemId, ...item };
-        const _id = generateListingId(doc);
-        return {
-          updateOne: {
-            filter: { _id },
-            update: { $setOnInsert: { _id, ...doc } },
-            upsert: true,
-          },
-        };
-      }),
-      { ordered: false },
-    );
+    const listings = await fetchAndProcessListings(45, 48493);
     console.table(
-      rawListings.map((item) => ({
+      listings.map((item) => ({
         ...item,
         price: item.price.toLocaleString(),
       })),
